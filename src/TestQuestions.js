c2bc76ff0f4457bbbfb8f93203600e0bcb4bd380
carmen <58992828+cgbl-90@users.noreply.github.com>
2022-09-21 23:07:22 +0200

    Update Order
    1. Created function updateOrder to identify the question being answered
    2. Tested getting data from JSON file

--- src/TestQuestions.js
+++ src/TestQuestions.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import data from "./test.json";
 import "./App.css";
 
 export default function TestQuestions() {
+  let [order, setOrder] = useState(0);
+  let [replies, setReplies] = useState(null);
+
+  function updateOrder(event) {
+    event.preventDefault();
+    setOrder(1);
+  }
+
+  if (order === 0) {
     return (
       <div className="card">
         <h1>
@@ -10,7 +20,15 @@ export default function TestQuestions() {
           We've got some React prep questions for you to prepare for your
           interview. Are you ready?
         </h1>
-        <button>Yes, let's do it!</button>
+        <button onClick={updateOrder}>Yes, let's do it!</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <h2>{data[order - 1].question}</h2>
+        <button>Next</button>
       </div>
     );
   }
+}
